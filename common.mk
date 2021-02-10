a86a6493b33a95a9e7cee57012503e0e34162cc4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:06:22 +0100

    sm6115-common: Build QTI Framework detect libs
    Change-Id: Ic4db96f8bd5a19f4805bd9c40544ed51d0352c61

--- common.mk
+++ common.mk
@@ -166,6 +166,14 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
