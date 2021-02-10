fe5f966a5ec2910996ed2893b5cc5c38a3b99318
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:09:03 +0100

    sm6115-common: Build QTI USB HIDL
    Change-Id: Id7da15ac872967e4fb733836c99efaf39aca3a56

--- common.mk
+++ common.mk
@@ -211,6 +211,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
-    
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm6115-common/sm6115-common-vendor.mk)
