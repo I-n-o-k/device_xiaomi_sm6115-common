099906498c8528df99d2e69cec69609d00e357f5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:02:15 +0100

    sm6115-common: Build libhwbinder
    * Some libs uses it
    
    Change-Id: Ic40f730a4d498725459785b8c33ef7f5e8d0b13b

--- common.mk
+++ common.mk
@@ -110,6 +110,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
