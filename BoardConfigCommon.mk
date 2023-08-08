66aa742dd2ce0548f626afc08e67e01effc3e524
I-n-o-k <inok.dr189@gmail.com>
2023-08-08 19:03:54 +0700

    sm6115-common: Add build rules flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,12 @@
 
 COMMON_PATH := device/xiaomi/sm6115-common
 
+# Build
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
+BUILD_BROKEN_VERIFY_USES_LIBRARIES := true
+RELAX_USES_LIBRARY_CHECK := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/sm6115-common/BoardConfigVendor.mk
