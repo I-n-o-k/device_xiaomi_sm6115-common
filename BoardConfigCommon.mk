66542e18c2bfd0a6c873f99fabc674133870d46c
Bruno Martins <bgcngm@gmail.com>
2024-06-16 15:14:57 +0100

    sm6115-common: Inherit from QTI and common Xiaomi FCMs
    Change-Id: I8d4d54bba1b3dff27c11b0034852697b818ee609
    Signed-off-by: HeroBuxx <me@herobuxx.me>
    Signed-off-by: I-n-o-k <inok.dr189@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,6 +64,8 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
     
 # Kernel
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
